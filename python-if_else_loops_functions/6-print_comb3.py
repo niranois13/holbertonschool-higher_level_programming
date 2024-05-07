ef008b90609046769c67ffb8acbf43a686d1920c
niranois13 <8784@holbertonstudents.com>
2024-05-07 22:12:47 +0200

    Task7 p3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-    if number // 10 == number % 10 or number // 10 == number % 10 * 10 + number // 10:
+    if number // 10 == number % 10:
+        continue
+    elif number // 10 == number % 10 * 10 + number // 10:
         continue
     else:
         print(f"{number:02d}")
