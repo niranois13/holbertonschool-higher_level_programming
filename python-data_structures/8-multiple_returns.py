bfc1f7d1aa319b46486321437c34f29f7fbc136d
niranois13 <8784@holbertonstudents.com>
2024-05-14 20:51:29 +0200

    8.

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != "":
-        length = len(sentence)
-        first = sentence[0]
-        tup = (length, first)
-        return tuple(tup)
+    length = len(sentence)
+    first = sentence[0] if sentence != "" else None
+    tup = (length, first)
+    return tuple(tup)
