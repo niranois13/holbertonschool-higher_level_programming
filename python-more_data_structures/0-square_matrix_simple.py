cfd5be29ecb78333188eb9d04f47573f4c16fd7e
niranois13 <8784@holbertonstudents.com>
2024-05-15 09:47:59 +0200

    0.

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+    for i , row in enumerate(new_matrix):
+        for j, value in enumerate(row):
+            new_matrix[i][j] = value ** 2
+    return new_matrix
