c869186e62a9a6fc15cbe23dab708fb58c054854
niranois13 <8784@holbertonstudents.com>
2024-05-17 15:25:59 +0200

    1.

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module compiled with python3"""
+
+
+class Square:
+    """"Class defining a Square, for now printing empty stuff"""
+    def __init__(self, size):
+        self.__size = size
