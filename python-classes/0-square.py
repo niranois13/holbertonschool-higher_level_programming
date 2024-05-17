e3c03629392262dc99af9e5aa6da6f8fbadb7f8b
niranois13 <8784@holbertonstudents.com>
2024-05-17 14:54:32 +0200

    Repo.0.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+"""Module compiled with python3"""
 class Square:
-    """"
-    Class defining a Square
-    """
+    """"Class defining a Square, for now printing empty stuff"""
     pass
