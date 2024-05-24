cc27b64b755eb71bf2e57c4af0d2c82eb3ed0875
niranois13 <8784@holbertonstudents.com>
2024-05-24 15:52:37 +0200

    9.

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -16,6 +16,8 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Returns a new Rectangle instance with width == height == size"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
         return cls(size, size)
 
     @property
