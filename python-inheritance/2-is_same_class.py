f6155eed48ed9fe0ed633c68b3e5b71bec7d23b6
niranois13 <8784@holbertonstudents.com>
2024-05-25 14:16:22 +0200

    2.

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -11,4 +11,4 @@ def is_same_class(obj, a_class):
 
     :Returns: True if obj is same class, False otherwise.
     """
-    return type(obj) == a_class
+    return True if type(obj) is a_class else False
