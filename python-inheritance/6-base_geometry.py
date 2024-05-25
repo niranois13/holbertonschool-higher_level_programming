06b2a95f3274f36f038672e8b10dabbbf95d3f73
niranois13 <8784@holbertonstudents.com>
2024-05-25 21:30:14 +0200

    6.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+
+class BaseGeometry:
+    """An emmpty class BaseGeometry"""
+
+    def area(self):
+        """Method not implemented yet"""
+        raise Exception("area() is not implemented")
