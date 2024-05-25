11caf915d0ebda13739c35ac42bb3369054f31e9
niranois13 <8784@holbertonstudents.com>
2024-05-25 14:27:14 +0200

    4.

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Method that checks if object is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+
+    :param obj: the object to check.
+    :param a_class: the class to compare with.
+
+    :Returns: True if obj is inherited from a_class, False otherwise.
+    """
+    return True if type(obj) is not a_class and isinstance(obj, a_class) else False
