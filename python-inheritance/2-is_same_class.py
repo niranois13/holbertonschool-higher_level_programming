2859f44b743de88825e08e87d7f7b5880d696481
niranois13 <8784@holbertonstudents.com>
2024-05-25 14:04:38 +0200

    2.

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Method to check if object is exactly an instance of the specified class
+    :param obj: the object to check
+    :param a_class: the class to compare with
+    :Returns: True if obj is same class, False otherwise
+    """
+    return True if type(obj) == a_class else False
