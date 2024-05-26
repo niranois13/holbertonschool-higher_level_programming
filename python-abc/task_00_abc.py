a91456915302b78667e629a4633bb3e5d19cd8ad
niranois13 <8784@holbertonstudents.com>
2024-05-26 10:19:34 +0200

    0.

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Defines the abstract class Animal, using the ABC module"""
+
+    @abstractmethod
+    def sound(self):
+        """Abstract method that prints the sound of given Animal"""
+        pass
+
+class Dog(Animal):
+    """Defines the subclass Dog, that inherits from superclass Animal"""
+
+    def sound(self):
+        """Method that returns a string of the sound of Dog"""
+        return ("Bark")
+
+class Cat(Animal):
+    """Defines the subclass Cat, that inherits from superclass Animal"""
+
+    def sound(self):
+        """Method that returns a string of the sound of Cat"""
+        return ("Meow")
