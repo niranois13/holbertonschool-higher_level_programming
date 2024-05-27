7d35a07cf2067fbffede5b8431a16bc1a24578d9
niranois13 <8784@holbertonstudents.com>
2024-05-27 10:18:37 +0200

    0.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """function that reads a text file (UTF-8) and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print("{}".format(read_data))
+        print(read_data, end="")
