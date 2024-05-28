b9b2411b82f5e1078d3dc54edf4af8de53e245b4
niranois13 <8784@holbertonstudents.com>
2024-05-28 15:51:09 +0200

    12.

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+
+def pascal_triangle(n):
+    """functionthat returns a list of lists of integers
+    representing the Pascal triangle of n"""
+    if n <= 0:
+        return []
