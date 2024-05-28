341ada203f60e826bcf0de23da3091853dd8c400
niranois13 <8784@holbertonstudents.com>
2024-05-28 15:37:04 +0200

    11.

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+
+class Student:
+    """Defines a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the class Student with first name, last name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method that retrieves a dict representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+
+        if not isinstance(attrs, list):
+            return {}
+
+        json_dict = {}
+        for item in attrs:
+            if not isinstance(item, str):
+                return {}
+            if hasattr(self, item):
+                json_dict[item] = getattr(self, item)
+        return json_dict
+
+    def reload_from_json(self, json):
+        """Method that replaces all attributes of the Student instance"""
+        for key, value in json:
+            setattr(self, key, value)
