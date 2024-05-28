fefc62a75012252fa55b4e59b6c7a6e2b43c3528
niranois13 <8784@holbertonstudents.com>
2024-05-28 16:21:49 +0200

    00

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module compiled by Python3"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Function that serializes and saves data to the specified file"""
+    with open(filename, 'w', encoding="UTF-8") as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """Function that loads and deserializes data from the specified file"""
+    with open(filename, 'r', encoding="UTF-8") as f:
+        data = json.loads(f)
+    return data
