440eabc2e16468e2564a97c6eebb77b22c6673d1
niranois13 <8784@holbertonstudents.com>
2024-06-01 14:00:06 +0200

    Setting up web server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """Module compiled with Python3"""
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, this is a simple API!')
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('content-type', 'application/json')
+            self.end_headers()
+            dataset = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(dataset).encode('UTF-8'))
 
 
 httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
