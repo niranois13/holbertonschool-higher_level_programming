708c24a001c506869e2f6016fc41ddda694279e4
niranois13 <8784@holbertonstudents.com>
2024-06-01 14:36:51 +0200

    03.

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,7 +5,10 @@ import json
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    """Defines the subclass SimpleHTTPRequestHandler,
+    that heritates from BaseHTTPRequestHandler"""
     def do_GET(self):
+        """Method that override the GET method, handles various endpoints"""
         if self.path == '/':
             self.send_response(200)
             self.end_headers()
@@ -16,9 +19,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             dataset = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(dataset).encode('UTF-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'OK')
+        elif self.path == '/info':
+            self.end_headers()
+            info = {"version": "1.0", "description":
+                    "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encore('UTF-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'404 - Not Found')
 
 
 httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
 print("serving at port", 8000)
 httpd.serve_forever()
-
