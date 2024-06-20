4ea55b4af468e59abe07e28dd7b7bc069bf48d66
niranois13 <8784@holbertonstudents.com>
2024-06-20 12:21:36 +0200

    SQL queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
-/* Lists rights of users */
+-- Lists rights of users
 
-/* User 01 */
+-- User 01
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-/* User 02 */
+-- User 02
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
