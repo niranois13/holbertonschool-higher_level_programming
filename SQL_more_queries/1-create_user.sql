e9a9794f9812e56164ff08727bf1e63aa15ffbd4
niranois13 <8784@holbertonstudents.com>
2024-06-20 15:06:21 +0200

    1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates and grants all privileges to user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANTS ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
