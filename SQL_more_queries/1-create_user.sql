85d72f24c2c6a3c8301554f588f995444793f442
niranois13 <8784@holbertonstudents.com>
2024-06-20 15:07:19 +0200

    1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates and grants all privileges to user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
