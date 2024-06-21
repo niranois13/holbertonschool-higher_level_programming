1528e3a08d7ec1cb3cd05d844467e1d40c5b84d9
niranois13 <8784@holbertonstudents.com>
2024-06-21 14:17:54 +0200

    00.

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module compiled with Python3"""
+
+import MySQLdb
+import sys
+
+def main():
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    main()
