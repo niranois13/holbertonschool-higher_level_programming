090c57184ad4568379d30bd140045d17eb8b6684
niranois13 <8784@holbertonstudents.com>
2024-07-16 12:12:23 +0200

    02

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items_file_path = os.path.join(os.path.dirname(__file__), 'items.json')
+    with open(items_file_path, 'r', encoding='UTF-8') as f:
+        data = json.load(f)
+
+    items = data.get('items', [])
+    return render_template('items.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
